d535f6514de82ee733eec94cb33813b1d8bb9a65
Gabriela Jorge <gferreirajorge@apple.com>
2024-10-18 16:14:04 -0300

    fix: add better error message for weak password

--- src/auth/dto/auth-credentials.dto.ts
+++ src/auth/dto/auth-credentials.dto.ts
@@ -9,6 +9,8 @@ export class AuthCredentialsDto {
   @IsString()
   @MinLength(8)
   @MaxLength(32)
-  @Matches(/((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/, { message: 'password is too weak' })
+  @Matches(/((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/, {
+    message: 'password needs at least: 1 upper case letter, 1 lower case, 1 number or special character',
+  })
   password: string;
 }
