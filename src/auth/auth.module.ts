fc39dc5382904f3d908c9bf2b287662532f9dc66
Gabriela Jorge <gferreirajorge@apple.com>
2024-10-18 17:35:45 -0300

    build: configure auth module to use passport and JWT

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -4,10 +4,21 @@ import { AuthController } from './auth.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './user.entity';
 import { UsersRepository } from './users.repository';
+import { PassportModule } from '@nestjs/passport';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
   controllers: [AuthController],
-  imports: [TypeOrmModule.forFeature([User])],
+  imports: [
+    PassportModule.register({ defaultStrategy: 'jwt' }),
+    JwtModule.register({
+      secret: 'etDeVarginha2000',
+      signOptions: {
+        expiresIn: 3600,
+      },
+    }),
+    TypeOrmModule.forFeature([User]),
+  ],
   providers: [AuthService, UsersRepository],
 })
 export class AuthModule {}
