2d9ce094282f65b20128da96ad23710b098e1805
Gabriela Jorge <gferreirajorge@apple.com>
2024-10-18 16:36:54 -0300

    feat: add password hashing

--- src/auth/users.repository.ts
+++ src/auth/users.repository.ts
@@ -3,6 +3,7 @@ import { User } from './user.entity';
 import { InjectRepository } from '@nestjs/typeorm';
 import { AuthCredentialsDto } from './dto/auth-credentials.dto';
 import { ConflictException, InternalServerErrorException } from '@nestjs/common';
+import * as bcrypt from 'bcrypt';
 
 export class UsersRepository extends Repository<User> {
   constructor(
@@ -15,9 +16,12 @@ export class UsersRepository extends Repository<User> {
   async createUser(authCredentialsDto: AuthCredentialsDto): Promise<void> {
     const { username, password } = authCredentialsDto;
 
+    const salt = await bcrypt.genSalt();
+    const hashedPassword = await bcrypt.hash(password, salt);
+
     const user = this.usersRepository.create({
       username,
-      password,
+      password: hashedPassword,
     });
 
     try {
